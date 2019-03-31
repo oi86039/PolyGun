90f8e06c172867ce2bae63b00a5b70c074bbf618
Justin <jbramweg509@gmail.com>
2019-03-31 15:36:16 -0400

    added enemy behavior (rough)

---
+++ Assets/enemyLazer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyLazer : MonoBehaviour
+{
+
+    public float speed;
+    // public Vector3 offset;
+    public GameObject player;
+    public int damage;
+    //private Rigidbody rb;
+
+    float timer;
+    public float TimeLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+        // offset = new Vector3(Random.Range(0, 0.5f), Random.Range(0, 0.5f), Random.Range(0, 0.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+
+        timer += Time.deltaTime;
+        if (timer >= TimeLimit)
+            Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.GetComponent<Player_Health>().health -= damage;
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/pixel_enemy.cs
+++ Assets/pixel_enemy.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum enemyID { SPUNK, SQUASH, STRETCH };
+
 public class pixel_enemy : MonoBehaviour
 {
+    public bool dead;
+
     public int cubes_Count; //Number of cubes in list 
     public int health;
+    public int damage;
     public float timeLimit;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    public float distThreshold;
+    public GameObject Player;
+    public GameObject evil_projectile;
+    float timer;
+    public float TimeLimit;
+
+    public float distance;
+
+    public enemyID id; //Different behaviors; 
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +39,91 @@ public class pixel_enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Behavior set
-        //Behavior function run
+        distance = Vector3.Distance(transform.position, Player.transform.position) ;
+        //Activate behavior if in proximity
+        if (distance <= distThreshold*2 && !dead) //If in proximity with player
+            //Behavior function run
+            Chase(id);
+        else
+            Behave(id);
+
+
         //Check if dead
         if (transform.childCount < cubes_Count / 2)
         { //if dead
           //Stop behaving
-          //Explode
+            dead = true;
+            //Explode
             foreach (Transform child in transform)
                 child.gameObject.GetComponent<pixel>().health = 0;
+        }
+        else if (transform.childCount <= 0)
+            Destroy(gameObject);
+    }
+
+    void Chase(enemyID id) //Actual enemy Update method
+    {
+        switch (id)
+        {
+            case enemyID.SPUNK:
+                transform.RotateAround(Player.transform.position,Vector3.up, horizontalSpeed); //Rotates Around Player
+                timer += Time.deltaTime;
+                if (timer >= TimeLimit) {
+                    //TimeLimit += Random.Range(-0.001, 0.1);
+                    horizontalSpeed *= -1;
+                    //Shoot
+                    GameObject instance = Instantiate(evil_projectile, transform.position, new Quaternion(0,0,0,0) );
+                    instance.GetComponent<enemyLazer>().damage = damage;
+                    instance.GetComponent<enemyLazer>().player = Player;
+                    timer = 0;
+                }
+                break;
+            case enemyID.SQUASH:
+
+                break;
+            case enemyID.STRETCH:
+                transform.RotateAround(Player.transform.position, Vector3.up, -horizontalSpeed); //Rotates Around Player
+                timer += Time.deltaTime;
+                if (timer >= TimeLimit)
+                {
+                   // TimeLimit += Random.Range(-1, 1);
+                    horizontalSpeed *= -1;
+                    //Shoot
+                    GameObject instance = Instantiate(evil_projectile, transform.position, new Quaternion(0, 0, 0, 0));
+                    instance.GetComponent<enemyLazer>().damage = damage;
+                    instance.GetComponent<enemyLazer>().player = Player;
+                    timer = 0;
+                }
+                transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, -verticalSpeed);
+                if (distance < distThreshold / 2 || distance > distThreshold)
+                    verticalSpeed*=-1;
+
+                break;
+        }
+    }    void Behave(enemyID id) //Actual enemy Update method
+    {
+        switch (id)
+        {
+            case enemyID.SPUNK:
+                transform.position += Vector3.right * (horizontalSpeed/15); //Rotates Around Player
+                timer += Time.deltaTime;
+                if (timer >= TimeLimit) {
+                    horizontalSpeed *= -1;
+                    timer = 0;
+                }
+                break;
+            case enemyID.SQUASH:
+
+                break;
+            case enemyID.STRETCH:
+                transform.position += Vector3.right * (horizontalSpeed/15); //Rotates Around Player
+                timer += Time.deltaTime;
+                if (timer >= TimeLimit)
+                {
+                    horizontalSpeed *= -1;
+                    timer = 0;
+                }
+                break;
         }
     }
 }
