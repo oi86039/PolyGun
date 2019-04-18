020892115547dfa01a724b7e3b41a7954ee0e47f
Michelle Anne Roxas <michanroxas@gmail.com>
2019-04-17 21:12:36 -0400

    Light up particle effects and removed particles from lasers. INTRODUCED BAD MEMORY LEAK, MUST FIX

--- Assets/__My Assets/_Scripts/gun.cs
+++ Assets/__My Assets/_Scripts/gun.cs
@@ -57,6 +57,7 @@ public class gun : MonoBehaviour
     public Text scoreText;
 
     [Header("Weapon")]
+    public Animator anim;
     public GameObject lazur;
     public FIRE_MODE firemode;
     public float fireRate;
@@ -201,6 +202,7 @@ public class gun : MonoBehaviour
         {
             case (FIRE_MODE.PISTOL):
                 firing = true;
+                anim.SetTrigger(0);
                 GameObject instance = Instantiate(lazur, spawnPos.position, spawnPos.rotation);
                 instance.GetComponent<lazur>().gun = gameObject;
                 //Play particle effect
